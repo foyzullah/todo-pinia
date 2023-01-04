e8865f2e1371c004b12e4299aeb2e41b4379b4f2
foyzullah <foyz.cse@gmail.com>
2023-01-04 09:26:29 +0600

    [ADD] tailwindcss and [DELETE] all unnecessary file

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+
+<template>
+  <div class="bg-gray-400 flex justify-center items-center h-screen">
+
+    <h2 class="text-center font-bold text-2xl">Now it is a fresh project file</h2>
+  </div>
+ 
+</template>
+
+<style scoped>
+
+</style>
